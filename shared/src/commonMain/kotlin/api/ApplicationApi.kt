544ed73f756742f1f873f9e433faabb1a4f54ad4
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-14 20:57:22 +0200

    Adds endpoint stubs

--- shared/src/commonMain/kotlin/api/ApplicationApi.kt
+++ shared/src/commonMain/kotlin/api/ApplicationApi.kt
@@ -1,12 +1,40 @@
 package api
 
+import api.model.*
 import io.ktor.client.*
 import kotlinx.coroutines.CoroutineScope
+import settings.SettingsRepository
+import util.Username
 
 interface ApplicationApi {
-    val token: String
+    val settings: SettingsRepository
     val scope: CoroutineScope
     val client: HttpClient
 
-    
+    suspend fun register(account: AccountRequest)
+    suspend fun login(account: LoginRequest)
+    suspend fun logout()
+
+    suspend fun getFriends()
+    suspend fun add(friend: Username)
+    suspend fun remove(friend: Username)
+
+    suspend fun getSentFriendRequests()
+    suspend fun getReceivedFriendRequests()
+    suspend fun sendFriendRequest(to: Username)
+    suspend fun cancelFriendRequest(to: Username) // TODO: This should be reworked on server side
+
+    suspend fun getBlockList()
+    suspend fun block(user: Username)
+    suspend fun unblock(user: Username)
+
+    suspend fun getGroupChats()
+    suspend fun createGroupChat(name: GroupChatNameRequest)
+
+    suspend fun getStatus()
+    suspend fun update(status: StatusRequest)
+
+    suspend fun update(password: UpdatePasswordRequest)
+    suspend fun update(username: UpdateUsernameRequest)
+    suspend fun deleteAccount(decision: Boolean)
 }
