6840449cb150ac373b2252b1aeedb9b7c831ffb5
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-17 14:40:53 +0200

    Updates lastOnline property to type Instant?

--- shared/src/commonMain/kotlin/api/model/FriendInfo.kt
+++ shared/src/commonMain/kotlin/api/model/FriendInfo.kt
@@ -1,5 +1,6 @@
 package api.model
 
+import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 import util.Username
 
@@ -7,5 +8,5 @@ import util.Username
 data class FriendInfo(
     val username: Username,
     val isOnline: Boolean,
-    val lastOnline: String?
+    val lastOnline: Instant?
 )
