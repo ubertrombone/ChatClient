deacbc70083da56557cc48d8f9d33d0aa46a7024
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-17 20:42:34 +0200

    Adds minimum length to usernames.

--- shared/src/commonMain/kotlin/util/Constants.kt
+++ shared/src/commonMain/kotlin/util/Constants.kt
@@ -1,10 +1,11 @@
 package util
 
 object Constants {
+    const val USERNAME_REQUIREMENT_MIN = 5
     const val PASSWORD_REQUIREMENT_MIN = 12
     const val REQUIREMENT_MAX = 24
     const val USERNAME_TOO_LONG = "Username must be no more than $REQUIREMENT_MAX characters!"
-    const val USERNAME_TOO_SHORT = "Username must contain at least 1 character!"
+    const val USERNAME_TOO_SHORT = "Username must contain at least $USERNAME_REQUIREMENT_MIN character!"
     const val INVALID_CHARS_USERNAME = "Username can only contain alpha-numeric characters!"
     const val PASSWORD_REQUIRES_CHARS = "Password must contain at least 1 letter and 1 number."
     const val INVALID_CHARS_PASSWORD = "Invalid characters!"
--- shared/src/commonMain/kotlin/util/Username.kt
+++ shared/src/commonMain/kotlin/util/Username.kt
@@ -3,6 +3,7 @@ package util
 import kotlinx.serialization.Serializable
 import util.Constants.INVALID_CHARS_USERNAME
 import util.Constants.REQUIREMENT_MAX
+import util.Constants.USERNAME_REQUIREMENT_MIN
 import util.Constants.USERNAME_TOO_LONG
 import util.Constants.USERNAME_TOO_SHORT
 import kotlin.jvm.JvmInline
@@ -11,7 +12,7 @@ import kotlin.jvm.JvmInline
 @JvmInline
 value class Username(val name: String) {
     init {
-        require(name.isNotEmpty()) { USERNAME_TOO_SHORT }
+        require(name.length >= USERNAME_REQUIREMENT_MIN) { USERNAME_TOO_SHORT }
         require(name.length <= REQUIREMENT_MAX) { "$USERNAME_TOO_LONG Length is: ${name.length}" }
         require(name.none { !it.isLetterOrDigit() }) { INVALID_CHARS_USERNAME }
     }
