066dc42a541451a1fcac39c279920abf3324ff48
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-17 14:45:19 +0200

    Removes unused constants.

--- shared/src/commonMain/kotlin/util/Constants.kt
+++ shared/src/commonMain/kotlin/util/Constants.kt
@@ -6,12 +6,8 @@ object Constants {
     const val USERNAME_TOO_LONG = "Username must be no more than $REQUIREMENT_MAX characters!"
     const val USERNAME_TOO_SHORT = "Username must contain at least 1 character!"
     const val INVALID_CHARS_USERNAME = "Username can only contain alpha-numeric characters!"
-    const val USERNAME_EXISTS = "Username already exists!"
     const val PASSWORD_REQUIRES_CHARS = "Password must contain at least 1 letter and 1 number."
     const val INVALID_CHARS_PASSWORD = "Invalid characters!"
     const val PASSWORD_SHORT = "Password must be at least $PASSWORD_REQUIREMENT_MIN characters!"
     const val PASSWORD_LONG = "Password must be no more than $REQUIREMENT_MAX characters!"
-    const val PASSWORDS_DONT_MATCH = "Passwords don't match"
-    const val INCORRECT_CREDS = "The username or password is incorrect"
-    const val INCORRECT_PASSWORD = "Incorrect password"
 }
