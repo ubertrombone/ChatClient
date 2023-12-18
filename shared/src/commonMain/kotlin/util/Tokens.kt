06c621fb3568a9dc6195401c9b981e8892942a79
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-18 21:17:12 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/util/Tokens.kt
@@ -0,0 +1,37 @@
+package util
+
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.OutlinedTextFieldDefaults.colors
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun textFieldColors(): TextFieldColors = colors(
+    focusedContainerColor = Color.Transparent,
+    focusedTextColor = colorScheme.primary,
+    focusedLabelColor = colorScheme.primary,
+    focusedLeadingIconColor = colorScheme.primary,
+    focusedTrailingIconColor = colorScheme.primary,
+    focusedBorderColor = colorScheme.primary,
+    unfocusedContainerColor = Color.Transparent,
+    unfocusedTextColor = colorScheme.secondary,
+    unfocusedLabelColor = colorScheme.secondary,
+    unfocusedLeadingIconColor = colorScheme.secondary,
+    unfocusedBorderColor = colorScheme.secondary,
+    unfocusedTrailingIconColor = colorScheme.secondary,
+    errorTextColor = colorScheme.error,
+    errorBorderColor = colorScheme.error,
+    errorContainerColor = Color.Transparent,
+    errorLeadingIconColor = colorScheme.error,
+    errorTrailingIconColor = colorScheme.error,
+    errorLabelColor = colorScheme.error,
+    errorCursorColor = colorScheme.error,
+    cursorColor = colorScheme.secondary
+)
+
+object ShapeTokens {
+    val roundedCorners = 24.dp
+}
