a80b7046f314647e05bdfab8df50def1b5edb0b2
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-19 20:26:24 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/components/PasswordField.kt
@@ -0,0 +1,51 @@
+package ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Outlined
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import util.ShapeTokens
+import util.textFieldColors
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun PasswordField(
+    password: String,
+    isError: Boolean,
+    modifier: Modifier = Modifier,
+    visibility: Boolean = false,
+    onValueChange: (String) -> Unit,
+    showPassword: (Boolean) -> Unit
+) {
+    OutlinedTextField(
+        value = password,
+        onValueChange = onValueChange,
+        label = { Text(text = "Username", fontSize = typography.labelMedium.fontSize) },
+        leadingIcon = { Icon(imageVector = Outlined.Lock, contentDescription = "Username") },
+        trailingIcon = {
+            Icon(
+                painter = painterResource(if (visibility) "visibility.xml" else "visibility_off.xml"),
+                contentDescription = "Show Password?",
+                modifier = Modifier.clickable { showPassword(!visibility) }
+            )               
+        },
+        visualTransformation = if (visibility) VisualTransformation.None else PasswordVisualTransformation(),
+        colors = textFieldColors(),
+        shape = RoundedCornerShape(ShapeTokens.roundedCorners),
+        singleLine = true,
+        isError = isError,
+        modifier = modifier
+    )
+}
