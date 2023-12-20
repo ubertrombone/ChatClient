36c92c879a12280be29fbd9a98e8614a82f69530
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-20 21:21:24 +0200

    Removes unused imports.

--- shared/src/commonMain/kotlin/ui/composables/PasswordField.kt
+++ shared/src/commonMain/kotlin/ui/composables/PasswordField.kt
@@ -3,9 +3,7 @@ package ui.composables
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.Icons.Outlined
-import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.typography
