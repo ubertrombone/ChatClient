741acd62ca11b89786ef2530c1346d2eddf29715
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-21 21:54:19 +0200

    Makes function compatible with Decompose; has onError pass error message to calling function.

--- shared/src/commonMain/kotlin/api/CallWrapper.kt
+++ shared/src/commonMain/kotlin/api/CallWrapper.kt
@@ -1,23 +1,25 @@
 package api
 
 import androidx.compose.runtime.MutableState
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.update
 import io.github.aakira.napier.Napier
 
 suspend fun <T> callWrapper(
-    isLoading: MutableState<Boolean>,
+    isLoading: MutableValue<Boolean>,
     operation: suspend () -> T,
     onSuccess: (T) -> Unit,
-    onError: (() -> Unit)?
+    onError: ((String) -> Unit)?
 ) {
-    isLoading.value = true
+    isLoading.update { true }
     try {
         val result = operation()
         onSuccess(result)
     } catch (e: Exception) {
         val error = e.message ?: "An unknown error has occurred."
         Napier.e(message = error, throwable = e)
-        onError?.invoke()
+        onError?.invoke(error)
     } finally {
-        isLoading.value = false
+        isLoading.update { false }
     }
 }
