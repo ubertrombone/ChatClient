7bee8630e725b443d39019630fc16e170948a4a3
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-22 21:53:46 +0200

    Refactors state arguments to be of type LoginAuthenticationFieldState; Adds Text components to display errors; implements loading and status arguments for all child composables.

--- shared/src/commonMain/kotlin/ui/login/LoginForm.kt
+++ shared/src/commonMain/kotlin/ui/login/LoginForm.kt
@@ -1,6 +1,7 @@
 package ui.login
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -11,35 +12,95 @@ import androidx.compose.ui.unit.dp
 import ui.composables.PasswordField
 import ui.composables.RememberMeCheckbox
 import ui.composables.UsernameField
-import ui.composables.states.AuthenticationFieldState
+import ui.composables.states.LoginAuthenticationFieldState
+import ui.composables.states.Types.PASSWORD
+import ui.composables.states.Types.USERNAME
+import util.Constants.NO_PASSWORD_PROVIDED
+import util.Status
+import util.Status.Error
+import util.Status.Success
 
 @Composable
 fun LoginForm(
-    usernameState: AuthenticationFieldState,
-    passwordState: AuthenticationFieldState,
+    usernameState: LoginAuthenticationFieldState,
+    passwordState: LoginAuthenticationFieldState,
     rememberMe: Boolean,
     modifier: Modifier = Modifier,
+    loginStatus: Status = Success,
+    isLoading: Boolean = false,
     onCheckChange: (Boolean) -> Unit,
     onButtonClick: () -> Unit
 ) {
+    usernameState.validateInput(loginStatus, type = USERNAME)
+    passwordState.validateInput(loginStatus, type = PASSWORD)
+
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        UsernameField(modifier = Modifier.width(300.dp), state = usernameState)
+        Text(
+            text = when {
+                loginStatus == Success -> ""
+                (loginStatus as Error).message == NO_PASSWORD_PROVIDED -> ""
+                else -> loginStatus.message
+            },
+            color = when {
+                loginStatus == Success -> colorScheme.background
+                (loginStatus as Error).message == NO_PASSWORD_PROVIDED -> colorScheme.background
+                else -> colorScheme.error
+            },
+            fontSize = typography.bodyLarge.fontSize,
+            fontWeight = typography.bodyLarge.fontWeight
+        )
 
-        Spacer(Modifier.height(50.dp))
+        Spacer(Modifier.height(20.dp))
 
-        PasswordField(modifier = Modifier.width(300.dp), state = passwordState)
+        UsernameField(
+            state = usernameState,
+            enabled = !isLoading,
+            modifier = Modifier.width(300.dp)
+        )
+
+        Spacer(Modifier.height(30.dp))
+
+        Text(
+            text = when {
+                loginStatus == Success -> ""
+                (loginStatus as Error).message == NO_PASSWORD_PROVIDED -> loginStatus.message
+                else -> ""
+            },
+            color = when {
+                loginStatus == Success -> colorScheme.background
+                (loginStatus as Error).message == NO_PASSWORD_PROVIDED -> colorScheme.error
+                else -> colorScheme.background
+            },
+            fontSize = typography.bodyLarge.fontSize,
+            fontWeight = typography.bodyLarge.fontWeight
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        PasswordField(
+            state = passwordState,
+            enabled = !isLoading,
+            modifier = Modifier.width(300.dp)
+        )
 
         Spacer(Modifier.height(15.dp))
 
-        RememberMeCheckbox(rememberMe = rememberMe, onCheckChanged = onCheckChange)
+        RememberMeCheckbox(
+            rememberMe = rememberMe,
+            enabled = !isLoading,
+            onCheckChanged = onCheckChange
+        )
 
         Spacer(Modifier.height(50.dp))
 
-        OutlinedButton(modifier = Modifier.width(160.dp), onClick = onButtonClick) {
+        OutlinedButton(
+            modifier = Modifier.width(160.dp),
+            onClick = onButtonClick
+        ) {
             Text(
                 text = "Login",
                 fontSize = typography.bodyLarge.fontSize,
