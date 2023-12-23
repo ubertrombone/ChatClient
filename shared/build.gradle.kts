02b8d33f7af72d9bf41b43835a7d5c3ddf326beb
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-23 13:14:52 +0200

    Removes unused plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.android.library")
     id("org.jetbrains.compose")
-    id("kotlin-parcelize")
     id("app.cash.sqldelight") version ("2.0.0")
 }
 
