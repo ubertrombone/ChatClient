234065a0435af472b122a0e0a163467b695ca0ea
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-23 13:31:42 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/register/RegisterContent.kt
@@ -0,0 +1,56 @@
+package ui.register
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import component.register.RegisterComponent
+import util.MainPhases
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterContent(component: RegisterComponent, modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = component.title,
+                        fontSize = typography.displayLarge.fontSize,
+                        fontWeight = typography.displayLarge.fontWeight,
+                    )
+                },
+                navigationIcon = {
+                    Icon(imageVector = Icons.Outlined.ArrowBack, contentDescription = "Return to Login")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorScheme.background,
+                    titleContentColor = colorScheme.primary
+                )
+            )
+        },
+        containerColor = colorScheme.background
+    ) {
+        // TODO: scrollable
+        Column(
+            modifier = Modifier.fillMaxSize().padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "REGISTER",
+                fontSize = typography.displayLarge.fontSize
+            )
+        }
+    }
+}
