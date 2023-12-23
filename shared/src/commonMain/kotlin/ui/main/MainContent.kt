ed62b7c17a42e10f087c0dbdc5146e940332ee55
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-23 12:45:34 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/main/MainContent.kt
@@ -0,0 +1,56 @@
+package ui.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import component.main.MainComponent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainContent(component: MainComponent, modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = component.title,
+                        fontSize = typography.displayLarge.fontSize,
+                        fontWeight = typography.displayLarge.fontWeight,
+                    )
+                },
+                actions = {// TODO: Temporary
+                    TextButton(
+                        modifier = Modifier.padding(end = 24.dp),
+                        onClick = component.onLogoutClicked
+                    ) {
+                        Text(
+                            text = "Logout",
+                            fontSize = typography.bodyMedium.fontSize,
+                            fontWeight = typography.bodyMedium.fontWeight
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorScheme.background,
+                    titleContentColor = colorScheme.primary
+                )
+            )
+        },
+        containerColor = colorScheme.background
+    ) {
+        Box(modifier = Modifier.fillMaxSize().padding(it), contentAlignment = Alignment.Center) {
+            Text(
+                text = "MAIN VIEW",
+                fontSize = typography.displayLarge.fontSize
+            )
+        }
+    }
+}
