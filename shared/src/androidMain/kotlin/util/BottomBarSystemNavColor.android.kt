440679ddf3d27ffe0101da359c1ce80bdd2f25ed
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-29 20:36:09 +0200

    Refactors function for actual use; function assigns a color to Android system nav bar.

---
+++ shared/src/androidMain/kotlin/util/BottomBarSystemNavColor.android.kt
@@ -0,0 +1,16 @@
+package util
+
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+
+@Composable
+actual fun BottomBarSystemNavColor(color: Color) {
+    val view = LocalView.current
+    if (!view.isInEditMode) SideEffect {
+        (view.context as Activity).window.navigationBarColor = color.toArgb()
+    }
+}
