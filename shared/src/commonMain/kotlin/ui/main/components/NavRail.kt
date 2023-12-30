5cb4f15bb7ace1f2640ce0c6b422c755f356010b
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-30 21:22:18 +0200

    Gets logout button to approximate NavigationRailItem

--- shared/src/commonMain/kotlin/ui/main/components/NavRail.kt
+++ shared/src/commonMain/kotlin/ui/main/components/NavRail.kt
@@ -1,19 +1,23 @@
 package ui.main.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.NavigationRail
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.MainComponent
@@ -33,12 +37,10 @@ fun NavRail(
     val childStack by component.childStack.subscribeAsState()
     val activeComponent = childStack.active.instance
 
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        NavigationRail(containerColor = colorScheme.primary) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(modifier = modifier) {
+        NavigationRail(modifier = Modifier.align(Alignment.TopCenter), containerColor = colorScheme.primary) {
             NavRailItem(
                 label = CHATS,
                 icon = { Icon(painter = painterResource("chat.xml"), contentDescription = "Friends list") },
@@ -68,11 +70,19 @@ fun NavRail(
             )
         }
 
-        IconButton(
-            modifier = Modifier.padding(end = 12.dp),
-            onClick = component::logout
-        ) {
-            Icon(imageVector = Icons.Default.ExitToApp, contentDescription = "Logout")
-        }
+        Icon(
+            imageVector = Icons.Default.ExitToApp,
+            contentDescription = "Logout",
+            tint = colorScheme.onPrimary,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .clip(CircleShape)
+                .clickable(
+                    indication = rememberRipple(color = colorScheme.primaryContainer),
+                    interactionSource = interactionSource,
+                    onClick = component::logout
+                )
+                .padding(horizontal = 16.dp, vertical = 4.dp)
+        )
     }
 }
