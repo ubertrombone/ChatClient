55349b70644ec669623b2e4ab60a4d24da552d4f
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-30 21:55:26 +0200

    Removes TODOs; reduces UI to a screen size check.

--- shared/src/commonMain/kotlin/ui/main/MainContent.kt
+++ shared/src/commonMain/kotlin/ui/main/MainContent.kt
@@ -1,14 +1,10 @@
 package ui.main
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme.typography
-import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass.Companion.Compact
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Expanded
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import component.main.MainComponent
 
@@ -17,18 +13,7 @@ import component.main.MainComponent
 fun MainContent(component: MainComponent, modifier: Modifier = Modifier) {
     val windowSizeClass = calculateWindowSizeClass()
 
-    // TODO:
-    //  Components:
-    //      3. friend list in lazy column
-    //          - immutable list returned by API
-    //          - clicking friend card navs to chat screen
-
     if (windowSizeClass.widthSizeClass == Expanded && windowSizeClass.heightSizeClass != Compact)
-        Box(modifier, contentAlignment = Alignment.Center) {
-            Text(
-                text = "EXPANDED VIEW",
-                fontSize = typography.displayLarge.fontSize
-            )
-        }
+        ExpandedScreen(component, modifier)
     else CompactScreen(component, modifier)
 }
