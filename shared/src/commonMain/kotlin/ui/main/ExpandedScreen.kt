9f866ea510e50170d289140436ce05bf3eb9a54a
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-30 21:42:00 +0200

    Fixes scoping issue when showing snackbar.

--- shared/src/commonMain/kotlin/ui/main/ExpandedScreen.kt
+++ shared/src/commonMain/kotlin/ui/main/ExpandedScreen.kt
@@ -6,42 +6,42 @@ import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import component.main.MainComponent
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import ui.main.components.ExpandedScreenChild
 import util.BottomBarSystemNavColor
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpandedScreen(component: MainComponent, modifier: Modifier = Modifier) {
     val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     BottomBarSystemNavColor(colorScheme.primary)
 
     Scaffold(
         modifier = modifier,
-        topBar = {
-            TopAppBar( // TODO: Make this an expect/actual fun? JVM doesn't need a title on every screen
-                title = {
-                    Text(
-                        text = component.title,
-                        fontSize = MaterialTheme.typography.displayLarge.fontSize,
-                        fontWeight = MaterialTheme.typography.displayLarge.fontWeight,
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = colorScheme.background,
-                    titleContentColor = colorScheme.primary
-                )
-            )
-        },
+//        topBar = {
+//            TopAppBar( // TODO: Make this an expect/actual fun? JVM doesn't need a title on every screen
+//                title = {
+//                    Text(
+//                        text = component.title,
+//                        fontSize = MaterialTheme.typography.displayLarge.fontSize,
+//                        fontWeight = MaterialTheme.typography.displayLarge.fontWeight,
+//                    )
+//                },
+//                colors = TopAppBarDefaults.topAppBarColors(
+//                    containerColor = colorScheme.background,
+//                    titleContentColor = colorScheme.primary
+//                )
+//            )
+//        },
         snackbarHost = { SnackbarHost(snackbarHostState) },
         containerColor = colorScheme.background
     ) {
         ExpandedScreenChild(component, Modifier.fillMaxSize().padding(it)) { message ->
-            runBlocking(Dispatchers.Main) { // TODO: I don't like this
+            scope.launch {
                 snackbarHostState.showSnackbar(
                     message = message,
                     actionLabel = "Dismiss",
