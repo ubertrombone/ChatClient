949a357572c395c7be3fe4b95e6dfda33cbc78e7
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-31 21:32:29 +0200

    Adds friend property

--- shared/src/commonMain/kotlin/component/main/friends/chat/ChatComponent.kt
+++ shared/src/commonMain/kotlin/component/main/friends/chat/ChatComponent.kt
@@ -1,10 +1,12 @@
 package component.main.friends.chat
 
 import api.ApplicationApi
+import api.model.FriendInfo
 import db.ChatRepository
 
 interface ChatComponent {
     val server: ApplicationApi
     val chatRepository: ChatRepository
     val cache: Boolean
+    val friend: FriendInfo
 }
--- shared/src/commonMain/kotlin/component/main/friends/chat/DefaultChatComponent.kt
+++ shared/src/commonMain/kotlin/component/main/friends/chat/DefaultChatComponent.kt
@@ -1,6 +1,7 @@
 package component.main.friends.chat
 
 import api.ApplicationApi
+import api.model.FriendInfo
 import com.arkivanov.decompose.ComponentContext
 import db.ChatRepository
 
@@ -8,5 +9,6 @@ class DefaultChatComponent(
     componentContext: ComponentContext,
     override val server: ApplicationApi,
     override val chatRepository: ChatRepository,
-    override val cache: Boolean
+    override val cache: Boolean,
+    override val friend: FriendInfo
 ) : ChatComponent, ComponentContext by componentContext
