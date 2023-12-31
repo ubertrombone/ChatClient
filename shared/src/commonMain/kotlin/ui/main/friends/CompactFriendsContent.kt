21d1a72b7667017b7aa6fbf69c50c5383bf0db3a
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-31 16:52:45 +0200

    Initial commit

---
+++ shared/src/commonMain/kotlin/ui/main/friends/CompactFriendsContent.kt
@@ -0,0 +1,10 @@
+package ui.main.friends
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import component.main.friends.FriendsComponent
+
+@Composable
+fun CompactFriendsContent(component: FriendsComponent, modifier: Modifier = Modifier) {
+
+}
