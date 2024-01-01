72ade038d214d74f3b76060a38d73843c5891b59
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-01 12:33:09 +0200

    Refactors FriendsContent to handle the UI Loading states and the Expanded and Compact composables to handle the actual content.

--- shared/src/commonMain/kotlin/ui/main/friends/ExpandedFriendsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/friends/ExpandedFriendsContent.kt
@@ -1,10 +1,53 @@
 package ui.main.friends
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.friends.FriendsComponent
 
 @Composable
 fun ExpandedFriendsContent(component: FriendsComponent, modifier: Modifier = Modifier) {
+    val friends by component.friends.subscribeAsState()
+    val chatSlot by component.chatSlot.subscribeAsState()
 
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        FriendsList(
+            modifier = Modifier.fillMaxHeight().weight(1f).padding(8.dp).background(Color.Cyan),
+            list = friends.friends,
+            friendSelected = component::showChat
+        )
+
+        Box(modifier = Modifier.fillMaxHeight().weight(4f).padding(8.dp).background(Color.Green)) {
+            chatSlot.child?.instance?.let {
+                Text(
+                    text = it.friend.username.name,
+                    fontSize = MaterialTheme.typography.displayLarge.fontSize,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+
+                IconButton(
+                    onClick = component::dismissChat,
+                    modifier = Modifier.align(Alignment.TopStart)
+                ) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Go back")
+                }
+            }
+        }
+    }
 }
--- shared/src/commonMain/kotlin/ui/main/friends/FriendsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/friends/FriendsContent.kt
@@ -1,19 +1,67 @@
 package ui.main.friends
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass.Companion.Compact
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Expanded
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.friends.FriendsComponent
+import kotlinx.coroutines.delay
+import util.Status
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun FriendsContent(component: FriendsComponent, modifier: Modifier = Modifier) {
     val windowSizeClass = calculateWindowSizeClass()
+    val status by component.status.subscribeAsState()
+    val isLoading by component.isLoading.subscribeAsState()
+    val friends by component.friends.subscribeAsState()
 
-    if (windowSizeClass.widthSizeClass == Expanded && windowSizeClass.heightSizeClass != Compact)
-        ExpandedFriendsContent(component, modifier)
-    else CompactFriendsContent(component, modifier)
+    // TODO: Needs to be periodically refreshing
+    LaunchedEffect(Unit) {
+        delay(500)
+        component.getFriends()
+    }
+
+    when {
+        isLoading -> CircularProgressIndicator(
+            modifier = Modifier.size(120.dp),
+            color = colorScheme.primary,
+            trackColor = colorScheme.surfaceVariant
+        )
+
+        status is Status.Error -> Box(modifier = modifier, contentAlignment = Alignment.Center) {
+            Text(
+                text = (status as Status.Error).message,
+                fontSize = typography.bodyLarge.fontSize,
+                color = Color.DarkGray
+            )
+        }
+
+        friends.friends.isEmpty() -> Box(modifier = modifier, contentAlignment = Alignment.Center) {
+            Text(
+                text = "You do not currently have any friends",
+                fontSize = typography.bodyLarge.fontSize,
+                color = Color.DarkGray
+            )
+        }
+
+        else ->
+            if (windowSizeClass.widthSizeClass == Expanded && windowSizeClass.heightSizeClass != Compact)
+                ExpandedFriendsContent(component, modifier)
+            else CompactFriendsContent(component, modifier)
+    }
 }
