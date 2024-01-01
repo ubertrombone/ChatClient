82f24af7b5aea122ae83d30bea7e7656c291c032
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-01 21:59:12 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/util/FormatTime.kt
@@ -0,0 +1,16 @@
+package util
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+
+fun Instant.ago() =
+    Clock.System.now().minus(this).toComponents { days, hours, minutes, seconds, _ ->
+        when {
+            days > 0 -> return@toComponents "$days ${"day".pluralizeTime(days.toInt())} ago"
+            hours > 0 -> return@toComponents "$hours ${"hour".pluralizeTime(hours)} ago"
+            minutes > 0 -> return@toComponents "$minutes ${"minute".pluralizeTime(minutes)} ago"
+            else -> return@toComponents "$seconds ${"second".pluralizeTime(seconds)} ago"
+        }
+    }
+
+private fun String.pluralizeTime(count: Int) = if (count > 1) "${this}s" else this
