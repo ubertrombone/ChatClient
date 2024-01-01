6fe558e824ff2706c7b49e732383bf6e7937edee
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-01 12:36:27 +0200

    Adds divider

--- shared/src/commonMain/kotlin/ui/main/friends/ExpandedFriendsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/friends/ExpandedFriendsContent.kt
@@ -1,18 +1,14 @@
 package ui.main.friends
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.friends.FriendsComponent
@@ -28,12 +24,18 @@ fun ExpandedFriendsContent(component: FriendsComponent, modifier: Modifier = Mod
         horizontalArrangement = Arrangement.Start
     ) {
         FriendsList(
-            modifier = Modifier.fillMaxHeight().weight(1f).padding(8.dp).background(Color.Cyan),
+            modifier = Modifier.fillMaxHeight().weight(1f).padding(8.dp),
             list = friends.friends,
             friendSelected = component::showChat
         )
 
-        Box(modifier = Modifier.fillMaxHeight().weight(4f).padding(8.dp).background(Color.Green)) {
+        Divider(
+            modifier = Modifier.fillMaxHeight().weight(.01f),
+            thickness = 0.01.dp,
+            color = colorScheme.scrim
+        )
+
+        Box(modifier = Modifier.fillMaxHeight().weight(4f).padding(8.dp)) {
             chatSlot.child?.instance?.let {
                 Text(
                     text = it.friend.username.name,
