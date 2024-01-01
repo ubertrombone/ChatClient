2ddc034ef7b5d010cba119c6ba1a13368e03cc24
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-01 13:00:53 +0200

    Adds Divider to separate friends in list.

--- shared/src/commonMain/kotlin/ui/main/friends/FriendsList.kt
+++ shared/src/commonMain/kotlin/ui/main/friends/FriendsList.kt
@@ -1,9 +1,16 @@
 package ui.main.friends
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import api.model.FriendInfo
 import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.toImmutableList
@@ -16,12 +23,25 @@ fun FriendsList(
     friendSelected: (FriendInfo) -> Unit
 ) {
     ScrollLazyColumn(modifier = modifier) {
-        items(items = list.toImmutableList()) {
-            FriendCard(
-                modifier = Modifier.fillMaxSize(),
-                friendInfo = it,
-                onClick = friendSelected
-            )
+        itemsIndexed(items = list.toImmutableList()) { index, item ->
+            Column(
+                modifier = Modifier,
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                FriendCard(
+                    modifier = Modifier.fillMaxSize(),
+                    friendInfo = item,
+                    onClick = friendSelected
+                )
+
+                if (index != list.indices.last) Divider(
+                    modifier = Modifier.fillMaxWidth(),
+                    thickness = 1.dp,
+                    color = Color.Gray
+                )
+            }
+
         }
     }
 }
