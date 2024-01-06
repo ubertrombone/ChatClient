e1085353fe60234ef2c960fccc3c856c40d3ed20
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-06 13:42:54 +0200

    Updates apiCall to reauthenticate and request a new token when username is changed.

--- shared/src/commonMain/kotlin/component/main/settings/implementation/UsernameModelImpl.kt
+++ shared/src/commonMain/kotlin/component/main/settings/implementation/UsernameModelImpl.kt
@@ -2,31 +2,47 @@ package component.main.settings.implementation
 
 import api.ApplicationApi
 import api.callWrapper
+import api.model.AuthenticationRequest
 import api.model.UpdateUsernameRequest
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.update
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
 import component.main.settings.interfaces.ApiModel
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import settings.SettingsRepository
 import util.Status
-import util.Status.Error
-import util.Status.Loading
+import util.Status.*
 import kotlin.coroutines.CoroutineContext
 
-class UsernameModelImpl(private val server: ApplicationApi) : ApiModel, InstanceKeeper.Instance {
+class UsernameModelImpl(
+    private val server: ApplicationApi,
+    private val settings: SettingsRepository
+) : ApiModel, InstanceKeeper.Instance {
+    private val scope = CoroutineScope(Dispatchers.IO)
     override val loadingState = MutableValue(false)
     override val status: MutableValue<Status> = MutableValue(Loading)
 
     override fun updateStatus(value: Status) { status.update { value } }
 
     override suspend fun <T> apiCall(value: T, context: CoroutineContext) = withContext(context) {
-        callWrapper(
-            isLoading = loadingState,
-            operation = { server.update(value as UpdateUsernameRequest) },
-            onSuccess = {
-                status.update { _ -> it }
-            },
-            onError = { status.update { _ -> Error(it) } }
-        )
+        with (value as UpdateUsernameRequest) {
+            callWrapper(
+                isLoading = loadingState,
+                operation = { server.update(this) },
+                onSuccess = {
+                    status.update { _ -> it }
+                    if (it == Success) scope.launch {
+                        server.authenticate(
+                            AuthenticationRequest(
+                                username = newUsername,
+                                password = settings.password.get()
+                            )
+                        )
+                            .also { auth -> if (auth == Success) settings.username.set(newUsername.name) }
+                    }
+                },
+                onError = { status.update { _ -> Error(it) } }
+            )
+        }
     }
 }
