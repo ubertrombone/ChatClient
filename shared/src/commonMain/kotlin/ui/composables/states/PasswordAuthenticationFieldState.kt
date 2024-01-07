82b15e680561c65ff5df2ad9bef5a0c51634a069
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-07 21:22:15 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/composables/states/PasswordAuthenticationFieldState.kt
@@ -0,0 +1,93 @@
+package ui.composables.states
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.decompose.value.update
+import kotlinx.coroutines.*
+import util.Constants.PASSWORDS_NOT_MATCH
+import util.Constants.PASSWORD_INCORRECT
+import util.Constants.PASSWORD_MUST_BE_NEW
+import util.Status
+import util.Status.Error
+import util.Status.Success
+import util.passwordToStatus
+
+class PasswordAuthenticationFieldState(
+    oldPassword: String = "",
+    newPassword: String = "",
+    confirmPassword: String = ""
+) {
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private val _oldPasswordInput = MutableValue(oldPassword)
+    val oldPasswordInput: Value<String> = _oldPasswordInput
+
+    private val _newPasswordInput = MutableValue(newPassword)
+    val newPasswordInput: Value<String> = _newPasswordInput
+
+    private val _confirmPasswordInput = MutableValue(confirmPassword)
+    val confirmPasswordInput: Value<String> = _confirmPasswordInput
+
+    private val _oldPasswordIsValid: MutableValue<Status> = MutableValue(Success)
+    val oldPasswordIsValid: Value<Status> = _oldPasswordIsValid
+
+    private val _newPasswordIsValid: MutableValue<Status> = MutableValue(Success)
+    val newPasswordIsValid: Value<Status> = _newPasswordIsValid
+
+    private val _confirmPasswordIsValid: MutableValue<Status> = MutableValue(Success)
+    val confirmPasswordIsValid: Value<Status> = _confirmPasswordIsValid
+
+    fun updateOldInput(with: String) = _oldPasswordInput.update { with }
+    fun updateNewInput(with: String) = _newPasswordInput.update { with }
+    fun updateConfirmInput(with: String) = _confirmPasswordInput.update { with }
+
+    fun validateInputs(password: String) {
+        scope.launch {
+            val oldPasswordIsValid = async {
+                _oldPasswordIsValid.update {
+                    if (_oldPasswordInput.value == password) Success else Error(PASSWORD_INCORRECT)
+                }
+            }
+
+            val newPasswordSameAsOld = async {
+                with(_newPasswordInput.value == password) {
+                    _newPasswordIsValid.update { if (this) Success else Error(PASSWORD_MUST_BE_NEW) }
+                }
+            }
+
+            val newPasswordIsPassword = async {
+                _newPasswordIsValid.update { _newPasswordInput.value.passwordToStatus() }
+            }
+
+            val newAndConfirmMatch = async {
+                with(_newPasswordInput.value == _confirmPasswordInput.value) {
+                    _newPasswordIsValid.update { if (this) Success else Error(PASSWORDS_NOT_MATCH) }
+                    _confirmPasswordIsValid.update { if (this) Success else Error(PASSWORDS_NOT_MATCH) }
+                }
+            }
+
+            awaitAll(oldPasswordIsValid, newPasswordSameAsOld, newPasswordIsPassword, newAndConfirmMatch)
+        }
+    }
+
+    companion object {
+        val saver = Saver<PasswordAuthenticationFieldState, List<Any>>(
+            save = { listOf(it._oldPasswordInput.value, it._newPasswordInput.value, it._confirmPasswordInput.value) },
+            restore = {
+                PasswordAuthenticationFieldState(
+                    oldPassword = it[0] as String,
+                    newPassword = it[1] as String,
+                    confirmPassword = it[2] as String
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberPasswordAuthenticationFieldState(): PasswordAuthenticationFieldState = rememberSaveable(
+    saver = PasswordAuthenticationFieldState.saver
+) { PasswordAuthenticationFieldState() }
---
+++ shared/src/commonMain/kotlin/ui/main/settings/UpdatePassword.kt
@@ -0,0 +1,13 @@
+package ui.main.settings
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ui.composables.states.PasswordAuthenticationFieldState
+
+@Composable
+fun UpdatePassword(
+    states: PasswordAuthenticationFieldState,
+    modifier: Modifier = Modifier
+) {
+
+}
