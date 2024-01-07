03f938384bda5fe9ed3b61d83b5eb0e60cd09265
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-07 14:40:10 +0200

    Moves label property into scope as well as onClick logic for SingleTextFieldRow.

--- shared/src/commonMain/kotlin/ui/main/settings/UpdateUsername.kt
+++ shared/src/commonMain/kotlin/ui/main/settings/UpdateUsername.kt
@@ -3,23 +3,23 @@ package ui.main.settings
 import androidx.compose.material.icons.Icons.Outlined
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.Icon
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import api.model.UpdateUsernameRequest
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import component.main.settings.SettingsComponent
 import kotlinx.coroutines.launch
 import ui.composables.states.UsernameAuthenticationFieldState
-import kotlin.coroutines.CoroutineContext
+import util.toUsername
 
 @Composable
 fun UpdateUsername(
+    component: SettingsComponent,
     usernameState: UsernameAuthenticationFieldState,
     currentUsername: String,
-    modifier: Modifier = Modifier,
-    label: String? = null,
-    onClick: suspend (CoroutineContext) -> Unit
+    modifier: Modifier = Modifier
 ) {
+    var label by remember { mutableStateOf<String?>(null) }
     val usernameInput by usernameState.input.subscribeAsState()
     val usernameValid by usernameState.isValid.subscribeAsState()
     val scope = rememberCoroutineScope()
@@ -29,8 +29,15 @@ fun UpdateUsername(
         label = label ?: "Update username",
         input = usernameInput,
         isError = !usernameValid || label != null,
-        enabled = currentUsername != usernameInput,
+        enabled = currentUsername.lowercase() != usernameInput.lowercase(),
         leadingIcon = { Icon(imageVector = Outlined.AccountCircle, contentDescription = "Username") },
         onInputChange = usernameState::updateInput
-    ) { scope.launch { onClick(this.coroutineContext) } }
+    ) {
+        scope.launch {
+            component.updateUsernameStatus(usernameState.validateInput())
+            if (usernameValid)
+                component.updateUsername(UpdateUsernameRequest(usernameInput.toUsername()), coroutineContext)
+            label = component.getUsernameAsResponse()
+        }
+    }
 }
