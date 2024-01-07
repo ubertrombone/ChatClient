55da9ef7362460ad1f5526f0fe8b78854ed874d7
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-07 13:16:07 +0200

    Refactors content to make more reusable.

--- shared/src/commonMain/kotlin/ui/main/settings/UpdateUsername.kt
+++ shared/src/commonMain/kotlin/ui/main/settings/UpdateUsername.kt
@@ -1,25 +1,15 @@
 package ui.main.settings
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.Icons.Outlined
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import kotlinx.coroutines.launch
 import ui.composables.states.UsernameAuthenticationFieldState
-import util.textFieldColors
 import kotlin.coroutines.CoroutineContext
 
 @Composable
@@ -34,33 +24,43 @@ fun UpdateUsername(
     val usernameValid by usernameState.isValid.subscribeAsState()
     val scope = rememberCoroutineScope()
 
-    Row(
+    SingleTextFieldRow(
         modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        TextField(
-            modifier = Modifier.weight(9f),
-            value = usernameInput,
-            onValueChange = usernameState::updateInput,
-            label = { Text(text = label ?: "Update username", fontSize = typography.labelMedium.fontSize) },
-            leadingIcon = { Icon(imageVector = Outlined.AccountCircle, contentDescription = "Username") },
-            isError = !usernameValid || label != null,
-            singleLine = true,
-            colors = textFieldColors()
-        )
+        label = label ?: "Update username",
+        input = usernameInput,
+        isError = !usernameValid || label != null,
+        enabled = currentUsername != usernameInput,
+        leadingIcon = { Icon(imageVector = Outlined.AccountCircle, contentDescription = "Username") },
+        onInputChange = usernameState::updateInput
+    ) { scope.launch { onClick(this.coroutineContext) } }
 
-        IconButton(
-            modifier = Modifier.size(40.dp),
-            onClick = { scope.launch { onClick(this.coroutineContext) } },
-            enabled = currentUsername != usernameInput,
-            colors = IconButtonDefaults.iconButtonColors(
-                containerColor = colorScheme.background,
-                contentColor = if (!usernameValid || label != null) colorScheme.error else colorScheme.primary,
-                disabledContentColor = colorScheme.onBackground
-            )
-        ) {
-            Icon(imageVector = Filled.CheckCircle, contentDescription = "Submit")
-        }
-    }
+//    Row(
+//        modifier = modifier,
+//        verticalAlignment = Alignment.CenterVertically,
+//        horizontalArrangement = Arrangement.SpaceBetween
+//    ) {
+//        TextField(
+//            modifier = Modifier.weight(9f),
+//            value = usernameInput,
+//            onValueChange = usernameState::updateInput,
+//            label = { Text(text = label ?: "Update username", fontSize = typography.labelMedium.fontSize) },
+//            leadingIcon = { Icon(imageVector = Outlined.AccountCircle, contentDescription = "Username") },
+//            isError = !usernameValid || label != null,
+//            singleLine = true,
+//            colors = textFieldColors()
+//        )
+//
+//        IconButton(
+//            modifier = Modifier.size(40.dp),
+//            onClick = { scope.launch { onClick(this.coroutineContext) } },
+//            enabled = currentUsername != usernameInput,
+//            colors = IconButtonDefaults.iconButtonColors(
+//                containerColor = colorScheme.background,
+//                contentColor = if (!usernameValid || label != null) colorScheme.error else colorScheme.primary,
+//                disabledContentColor = colorScheme.onBackground
+//            )
+//        ) {
+//            Icon(imageVector = Filled.CheckCircle, contentDescription = "Submit")
+//        }
+//    }
 }
