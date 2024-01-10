fb77414d9e08de806754d55338f13f936e900bc1
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-10 12:13:04 +0200

    Implements ReceivedRequestsComponent

--- shared/src/commonMain/kotlin/component/main/add/requests/received/DefaultReceivedRequestsComponent.kt
+++ shared/src/commonMain/kotlin/component/main/add/requests/received/DefaultReceivedRequestsComponent.kt
@@ -1,7 +1,62 @@
 package component.main.add.requests.received
 
+import api.ApplicationApi
+import api.model.FriendRequest
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.essenty.instancekeeper.getOrCreate
+import component.main.add.model.Requests
+import kotlinx.serialization.builtins.serializer
+import util.Status
+import util.Status.Loading
+import util.Status.Success
 
 class DefaultReceivedRequestsComponent(
-    componentContext: ComponentContext
-) : ReceivedRequestsComponent, ComponentContext by componentContext
+    componentContext: ComponentContext,
+    override val server: ApplicationApi,
+    override val navToSent: () -> Unit,
+    override val dismiss: () -> Unit,
+    override val logout: () -> Unit
+) : ReceivedRequestsComponent, ComponentContext by componentContext {
+    private val _getRequestStates = instanceKeeper.getOrCreate(RECEIVED_KEY) {
+        ReceiveListModel(
+            initialLoadingState = stateKeeper.consume(key = LOADING_KEY, strategy = Boolean.serializer()) ?: true,
+            initialStatus = stateKeeper.consume(key = STATUS_KEY, strategy = Status.serializer()) ?: Loading,
+            initialState = stateKeeper.consume(key = RECEIVED_KEY, strategy = Requests.serializer()) ?: Requests(),
+            server = server,
+            authCallback = logout
+        )
+    }
+    override val listStatus: Value<Status> = _getRequestStates.status
+    override val listLoading: Value<Boolean> = _getRequestStates.loadingState
+    override val receivedList: Value<Requests> = _getRequestStates.result
+
+    private val _acceptRequestStates = instanceKeeper.getOrCreate {
+        AcceptModel(
+            initialLoadingState = false,
+            initialStatus = Success,
+            server = server,
+            authCallback = logout
+        )
+    }
+    override val actionStatus: Value<Status> = _acceptRequestStates.status
+    override val actionLoading: Value<Boolean> = _acceptRequestStates.loadingState
+
+    override fun getRequests() = _getRequestStates.getRequests()
+
+    override fun acceptRequest(request: FriendRequest) = _acceptRequestStates.addFriend(request)
+
+    override fun rejectRequest(request: FriendRequest) = _acceptRequestStates.closeRequest(request)
+
+    init {
+        stateKeeper.register(key = RECEIVED_KEY, strategy = Requests.serializer()) { _getRequestStates.result.value }
+        stateKeeper.register(key = LOADING_KEY, strategy = Boolean.serializer()) { _getRequestStates.loadingState.value }
+        stateKeeper.register(key = STATUS_KEY, strategy = Status.serializer()) { _getRequestStates.status.value }
+    }
+
+    private companion object {
+        const val RECEIVED_KEY = "RECEIVED_KEY"
+        const val LOADING_KEY = "LOADING_KEY"
+        const val STATUS_KEY = "STATUS_KEY"
+    }
+}
--- shared/src/commonMain/kotlin/component/main/add/requests/received/ReceivedRequestsComponent.kt
+++ shared/src/commonMain/kotlin/component/main/add/requests/received/ReceivedRequestsComponent.kt
@@ -1,5 +1,25 @@
 package component.main.add.requests.received
 
-interface ReceivedRequestsComponent {
+import api.ApplicationApi
+import api.model.FriendRequest
+import com.arkivanov.decompose.value.Value
+import component.main.add.model.Requests
+import util.Status
 
+interface ReceivedRequestsComponent {
+    val server: ApplicationApi
+    val navToSent: () -> Unit
+    val dismiss: () -> Unit
+    val logout: () -> Unit
+
+    val listStatus: Value<Status>
+    val listLoading: Value<Boolean>
+    val receivedList: Value<Requests>
+
+    val actionStatus: Value<Status>
+    val actionLoading: Value<Boolean>
+
+    fun getRequests()
+    fun acceptRequest(request: FriendRequest)
+    fun rejectRequest(request: FriendRequest)
 }
