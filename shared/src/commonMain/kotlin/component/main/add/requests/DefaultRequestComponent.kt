712df9a2a7564247c1cdeae6b71b21d54fc80c15
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-10 11:41:03 +0200

    Implements infra

--- shared/src/commonMain/kotlin/component/main/add/requests/DefaultRequestComponent.kt
+++ shared/src/commonMain/kotlin/component/main/add/requests/DefaultRequestComponent.kt
@@ -1,7 +1,63 @@
 package component.main.add.requests
 
+import api.ApplicationApi
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.*
+import com.arkivanov.decompose.value.Value
+import component.main.add.requests.DefaultRequestComponent.Config.Received
+import component.main.add.requests.DefaultRequestComponent.Config.Sent
+import component.main.add.requests.RequestComponent.Child
+import component.main.add.requests.RequestComponent.Child.ReceivedChild
+import component.main.add.requests.RequestComponent.Child.SentChild
+import component.main.add.requests.received.DefaultReceivedRequestsComponent
+import component.main.add.requests.sent.DefaultSentRequestsComponent
+import kotlinx.serialization.Serializable
 
 class DefaultRequestComponent(
-    componentContext: ComponentContext
-) : RequestComponent, ComponentContext by componentContext
+    componentContext: ComponentContext,
+    override val server: ApplicationApi,
+    override val dismiss: () -> Unit,
+    override val logout: () -> Unit
+) : RequestComponent, ComponentContext by componentContext {
+    private val navigation = StackNavigation<Config>()
+    private val _childStack = childStack(
+        source = navigation,
+        serializer = Config.serializer(),
+        initialConfiguration = Received,
+        handleBackButton = true,
+        childFactory = ::createChild
+    )
+    override val childStack: Value<ChildStack<*, Child>> = _childStack
+
+    private fun createChild(
+        config: Config,
+        componentContext: ComponentContext
+    ) = when (config) {
+        Received -> SentChild(sentChild(componentContext))
+        Sent -> ReceivedChild(receivedChild(componentContext))
+    }
+
+    private fun sentChild(componentContext: ComponentContext) = DefaultSentRequestsComponent(
+        componentContext = componentContext,
+        server = server,
+        navBack = ::navBack,
+        dismiss = dismiss,
+        logout = logout
+    )
+
+    private fun receivedChild(componentContext: ComponentContext) = DefaultReceivedRequestsComponent(
+        componentContext = componentContext
+    )
+
+    override fun onSentRequestsSelected() = navigation.bringToFront(Sent)
+
+    override fun onReceivedRequestsSelected() = navigation.bringToFront(Received)
+
+    override fun navBack() = navigation.pop()
+
+    @Serializable
+    private sealed class Config {
+        @Serializable data object Sent : Config()
+        @Serializable data object Received : Config()
+    }
+}
--- shared/src/commonMain/kotlin/component/main/add/requests/RequestComponent.kt
+++ shared/src/commonMain/kotlin/component/main/add/requests/RequestComponent.kt
@@ -1,4 +1,29 @@
 package component.main.add.requests
 
+import api.ApplicationApi
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import component.main.add.requests.received.ReceivedRequestsComponent
+import component.main.add.requests.sent.SentRequestsComponent
+
 interface RequestComponent {
+    val server: ApplicationApi
+    val dismiss: () -> Unit
+    val logout: () -> Unit
+
+    val childStack: Value<ChildStack<*, Child>>
+
+    fun onSentRequestsSelected()
+    fun onReceivedRequestsSelected()
+    fun navBack()
+
+    sealed class Child {
+        class SentChild(val component: SentRequestsComponent) : Child()
+        class ReceivedChild(val component: ReceivedRequestsComponent) : Child()
+    }
+    // TODO: 3. Requests
+    //  GET /friend_request/sent_friend_requests - see the list of friends requests a user has SENT
+    //  POST /friend_request/cancel_request - remove a friend request you've sent
+    //  GET /friend_request/received_friend_requests - list of friend requests a user has RECEIVED
+    //  POST /friend_request - action to accept or reject a received friend request
 }
