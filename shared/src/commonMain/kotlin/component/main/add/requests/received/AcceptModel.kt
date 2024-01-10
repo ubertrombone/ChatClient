ea8dad522e7a5a179c1b48fe76030250ec9dd578
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-10 12:12:30 +0200

    Finalizes API call order.

--- shared/src/commonMain/kotlin/component/main/add/requests/received/AcceptModel.kt
+++ shared/src/commonMain/kotlin/component/main/add/requests/received/AcceptModel.kt
@@ -2,16 +2,18 @@ package component.main.add.requests.received
 
 import api.ApplicationApi
 import api.callWrapper
+import api.model.FriendRequest
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.update
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
-import io.ktor.http.*
+import io.ktor.http.HttpStatusCode.Companion.Unauthorized
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import util.Status
-import util.Username
+import util.Status.Error
+import util.Status.Success
 
 class AcceptModel(
     initialLoadingState: Boolean,
@@ -23,15 +25,32 @@ class AcceptModel(
     val loadingState = MutableValue(initialLoadingState)
     val status = MutableValue(initialStatus)
 
-    fun receiveRequest(username: Username, accept: Boolean) {
+    fun addFriend(request: FriendRequest) {
         scope.launch {
             callWrapper(
                 isLoading = loadingState,
-                operation = {  },
+                operation = { server.add(request.requesterUsername) },
+                onSuccess = { result ->
+                    if (result == Success) scope.launch { closeRequest(request) }
+                    else status.update { result }
+                },
+                onError = {
+                    status.update { _ -> Error(it) }
+                    if (it == Unauthorized.description) authCallback()
+                }
+            )
+        }
+    }
+
+    fun closeRequest(request: FriendRequest) {
+        scope.launch {
+            callWrapper(
+                isLoading = loadingState,
+                operation = { server.removeFriendRequest(request) },
                 onSuccess = { status.update { _ -> it } },
                 onError = {
-                    status.update { _ -> Status.Error(it) }
-                    if (it == HttpStatusCode.Unauthorized.description) authCallback()
+                    status.update { _ -> Error(it) }
+                    if (it == Unauthorized.description) authCallback()
                 }
             )
         }
