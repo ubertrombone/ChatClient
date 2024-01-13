478aebb125f17882a5a8e5c4cf4f487660279e56
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-13 13:53:43 +0200

    Adds TopBar to handle navigation

--- shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
@@ -2,21 +2,23 @@ package ui.main.add.requests
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.RequestComponent
 import component.main.add.requests.RequestComponent.Child.ReceivedChild
 import component.main.add.requests.RequestComponent.Child.SentChild
+import ui.composables.NavBackButton
 import ui.main.add.requests.received.ReceivedContent
 import ui.main.add.requests.sent.SentContent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RequestsContent(component: RequestComponent, modifier: Modifier = Modifier) {
     val childStack by component.childStack.subscribeAsState()
@@ -24,6 +26,21 @@ fun RequestsContent(component: RequestComponent, modifier: Modifier = Modifier) 
 
     Scaffold(
         modifier = modifier,
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.padding(bottom = 12.dp),
+                title = { Text(
+                    text = "Requests",
+                    fontSize = typography.headlineLarge.fontSize,
+                    fontWeight = typography.headlineLarge.fontWeight
+                ) },
+                navigationIcon = { NavBackButton { component.dismiss() } },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorScheme.background,
+                    titleContentColor = colorScheme.primary
+                )
+            )
+        },
         snackbarHost = {
             SnackbarHost(snackbarHostState) { data ->
                 Snackbar(
