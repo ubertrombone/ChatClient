0d0f6a2e3aed5b05a16f147fe19a8a1d1079a8e8
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-13 13:55:36 +0200

    Makes nav button a search icon instead of awkward back arrow

--- shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
@@ -1,20 +1,24 @@
 package ui.main.add.requests
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.RequestComponent
 import component.main.add.requests.RequestComponent.Child.ReceivedChild
 import component.main.add.requests.RequestComponent.Child.SentChild
-import ui.composables.NavBackButton
 import ui.main.add.requests.received.ReceivedContent
 import ui.main.add.requests.sent.SentContent
 
@@ -29,12 +33,20 @@ fun RequestsContent(component: RequestComponent, modifier: Modifier = Modifier) 
         topBar = {
             CenterAlignedTopAppBar(
                 modifier = Modifier.padding(bottom = 12.dp),
-                title = { Text(
-                    text = "Requests",
-                    fontSize = typography.headlineLarge.fontSize,
-                    fontWeight = typography.headlineLarge.fontWeight
-                ) },
-                navigationIcon = { NavBackButton { component.dismiss() } },
+                title = {},
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Search,
+                        contentDescription = "Return to previous page",
+                        tint = colorScheme.primary,
+                        modifier = Modifier
+                            .padding(start = 12.dp, top = 12.dp)
+                            .size(40.dp)
+                            .padding(5.dp)
+                            .clip(CircleShape)
+                            .clickable { component.dismiss() }
+                    )
+                },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = colorScheme.background,
                     titleContentColor = colorScheme.primary
