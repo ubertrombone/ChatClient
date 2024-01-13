4139627fecf58acaed8b9e6571ddc5c6def7dc6d
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-13 13:04:53 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/main/add/requests/sent/SentContent.kt
@@ -0,0 +1,76 @@
+package ui.main.add.requests.sent
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import component.main.add.requests.sent.SentRequestsComponent
+import kotlinx.collections.immutable.toImmutableList
+import ui.composables.expect.ScrollLazyColumn
+import ui.main.add.AddCard
+import util.Status.Error
+import util.Status.Success
+
+@Composable
+fun SentContent(component: SentRequestsComponent, modifier: Modifier = Modifier) {
+    val requests by component.sentList.subscribeAsState()
+    val sentStatus by component.listStatus.subscribeAsState()
+    val sentLoading by component.listLoading.subscribeAsState()
+
+    LaunchedEffect(requests) { component.getSentList() }
+
+    Box(modifier = modifier) {
+        when {
+            sentLoading -> CircularProgressIndicator(
+                modifier = Modifier.size(120.dp).align(Alignment.Center),
+                color = colorScheme.primary,
+                trackColor = colorScheme.surfaceVariant
+            )
+
+            sentStatus is Error -> Text(
+                text = (sentStatus as Error).body.toString(),
+                modifier = Modifier.align(Alignment.Center)
+            )
+
+            sentStatus == Success -> {
+                if (requests.reqs.isEmpty()) Text(
+                    text = "You haven't sent any requests.",
+                    modifier = Modifier.align(Alignment.Center)
+                ) else ScrollLazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    items(requests.reqs.toImmutableList()) {
+                        AddCard(
+                            label = "Sent to ${it.toUsername}",
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            IconButton(
+                                modifier = Modifier.padding(end = 24.dp),
+                                onClick = { component.cancelRequest(it.toUsername) }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Delete,
+                                    contentDescription = "Cancel request",
+                                    tint = colorScheme.primary
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
