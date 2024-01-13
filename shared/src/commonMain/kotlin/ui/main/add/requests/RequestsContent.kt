c1d1f53f61f9c8ed370530a0a0e8167bae714d45
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-13 13:23:36 +0200

    Adds child content

--- shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
@@ -1,6 +1,7 @@
 package ui.main.add.requests
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -9,6 +10,8 @@ import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.RequestComponent
 import component.main.add.requests.RequestComponent.Child.ReceivedChild
 import component.main.add.requests.RequestComponent.Child.SentChild
+import ui.main.add.requests.received.ReceivedContent
+import ui.main.add.requests.sent.SentContent
 
 @Composable
 fun RequestsContent(component: RequestComponent, modifier: Modifier = Modifier) {
@@ -17,8 +20,8 @@ fun RequestsContent(component: RequestComponent, modifier: Modifier = Modifier) 
     Box(modifier) {
         Children(childStack) {
             when (val child = it.instance) {
-                is ReceivedChild -> TODO()
-                is SentChild -> TODO()
+                is ReceivedChild -> ReceivedContent(component = child.component, modifier = Modifier.fillMaxSize())
+                is SentChild -> SentContent(component = child.component, modifier = Modifier.fillMaxSize())
             }
         }
     }
