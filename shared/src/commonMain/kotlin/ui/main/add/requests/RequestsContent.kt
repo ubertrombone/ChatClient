1ad15588ceb58623fc5d1fd5cf3fe0bdef19095e
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-14 20:52:49 +0200

    Removes scaffold

--- shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/RequestsContent.kt
@@ -1,19 +1,10 @@
 package ui.main.add.requests
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.RequestComponent
@@ -22,53 +13,15 @@ import component.main.add.requests.RequestComponent.Child.SentChild
 import ui.main.add.requests.received.ReceivedContent
 import ui.main.add.requests.sent.SentContent
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RequestsContent(component: RequestComponent, modifier: Modifier = Modifier) {
     val childStack by component.childStack.subscribeAsState()
-    val snackbarHostState = component.snackbarHostState
 
-    Scaffold(
-        modifier = modifier,
-        topBar = {
-            CenterAlignedTopAppBar(
-                modifier = Modifier.padding(bottom = 12.dp),
-                title = {},
-                navigationIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Search,
-                        contentDescription = "Return to previous page",
-                        tint = colorScheme.primary,
-                        modifier = Modifier
-                            .padding(start = 12.dp, top = 12.dp)
-                            .size(40.dp)
-                            .padding(5.dp)
-                            .clip(CircleShape)
-                            .clickable { component.dismiss() }
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = colorScheme.background,
-                    titleContentColor = colorScheme.primary
-                )
-            )
-        },
-        snackbarHost = {
-            SnackbarHost(snackbarHostState) { data ->
-                Snackbar(
-                    snackbarData = data,
-                    containerColor = colorScheme.primaryContainer.copy(alpha = .7f), // TODO: Something wrong here in light mode
-                    contentColor = colorScheme.onPrimaryContainer,
-                    dismissActionContentColor = colorScheme.onPrimaryContainer
-                )
-            }
-        },
-        containerColor = colorScheme.background
-    ) { padding ->
+    Box(modifier) {
         Children(childStack) {
             when (val child = it.instance) {
-                is ReceivedChild -> ReceivedContent(component = child.component, modifier = Modifier.fillMaxSize().padding(padding))
-                is SentChild -> SentContent(component = child.component, modifier = Modifier.fillMaxSize().padding(padding))
+                is ReceivedChild -> ReceivedContent(component = child.component, modifier = Modifier.fillMaxSize())
+                is SentChild -> SentContent(component = child.component, modifier = Modifier.fillMaxSize())
             }
         }
     }
