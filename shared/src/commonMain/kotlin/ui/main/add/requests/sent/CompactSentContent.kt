0f2fddeebfcbaa5b642cac5565c1af30aa6e1c41
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-18 21:39:32 +0200

    Hoists content to SentContent

--- shared/src/commonMain/kotlin/ui/main/add/requests/sent/CompactSentContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/sent/CompactSentContent.kt
@@ -1,41 +1,23 @@
 package ui.main.add.requests.sent
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.sent.SentRequestsComponent
-import kotlinx.collections.immutable.toImmutableList
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import ui.composables.expect.ScrollLazyColumn
 import ui.icons.ReceivedRequestsIcon
 import ui.icons.SearchIcon
-import ui.main.add.AddCard
-import util.Status.Error
-import util.Status.Success
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalResourceApi::class)
 @Composable
 fun CompactSentContent(component: SentRequestsComponent, modifier: Modifier = Modifier) {
-    val snackbarHostState = component.snackbarHostState
-    val requests by component.sentList.subscribeAsState()
-    val sentStatus by component.listStatus.subscribeAsState()
-    val sentLoading by component.listLoading.subscribeAsState()
-    val cancelStatus by component.actionStatus.subscribeAsState()
-
-    // TODO: Test
-    LaunchedEffect(requests) { component.getSentList() }
-    LaunchedEffect(cancelStatus) { if (cancelStatus == Success) component.getSentList() }
-
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -56,56 +38,6 @@ fun CompactSentContent(component: SentRequestsComponent, modifier: Modifier = Mo
                 )
             )
         },
-        snackbarHost = {
-            SnackbarHost(snackbarHostState) { data ->
-                Snackbar(
-                    snackbarData = data,
-                    containerColor = colorScheme.primaryContainer.copy(alpha = .7f), // TODO: Something wrong here in light mode
-                    contentColor = colorScheme.onPrimaryContainer,
-                    dismissActionContentColor = colorScheme.onPrimaryContainer
-                )
-            }
-        },
         containerColor = colorScheme.background
-    ) { padding ->
-        Box(modifier = Modifier.fillMaxSize().padding(padding)) {
-            when {
-                sentLoading -> CircularProgressIndicator(
-                    modifier = Modifier.size(120.dp).align(Alignment.Center),
-                    color = colorScheme.primary,
-                    trackColor = colorScheme.surfaceVariant
-                )
-
-                sentStatus is Error -> Text(
-                    text = (sentStatus as Error).body.toString(),
-                    modifier = Modifier.align(Alignment.Center)
-                )
-
-                sentStatus == Success -> {
-                    if (requests.reqs.isEmpty()) Text(
-                        text = "You haven't sent any requests.",
-                        modifier = Modifier.align(Alignment.Center)
-                    ) else ScrollLazyColumn(modifier = Modifier.fillMaxWidth()) {
-                        items(requests.reqs.toImmutableList()) {
-                            AddCard(
-                                label = "Sent to ${it.toUsername.name}",
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                IconButton(
-                                    modifier = Modifier.padding(end = 24.dp),
-                                    onClick = { component.cancelRequest(it.toUsername) }
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Default.Delete,
-                                        contentDescription = "Cancel request",
-                                        tint = colorScheme.primary
-                                    )
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    ) { SentContent(component = component, modifier = Modifier.fillMaxSize().padding(it)) }
 }
