1ecb3c96c7fba411de34a10830572f33ceabf5fb
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-18 22:22:46 +0200

    Implements RequestSwitch

--- shared/src/commonMain/kotlin/ui/main/add/requests/received/ReceivedContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/received/ReceivedContent.kt
@@ -1,30 +1,27 @@
 package ui.main.add.requests.received
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.received.ReceivedRequestsComponent
 import io.ktor.client.statement.*
 import kotlinx.collections.immutable.toImmutableList
-import ui.composables.expect.ScrollLazyColumn
 import ui.composables.snackbarHelper
 import ui.main.add.AddCard
+import ui.main.add.requests.RequestSwitch
 import util.Status.Error
-import util.Status.Success
 
 @Composable
 fun ReceivedContent(
@@ -43,53 +40,37 @@ fun ReceivedContent(
         )
     }
 
-    Box(modifier) {
-        when {
-            receivedLoading -> CircularProgressIndicator(
-                modifier = Modifier.size(120.dp).align(Alignment.Center),
-                color = colorScheme.primary,
-                trackColor = colorScheme.surfaceVariant
-            )
+    RequestSwitch(
+        label = "You haven't received any requests.",
+        results = requests.reqs.toImmutableList(),
+        status = receivedStatus,
+        loading = receivedLoading,
+        modifier = modifier
+    ) {
+        AddCard(
+            label = "${it.requesterUsername.name} would like to add you!",
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(
+                modifier = Modifier.padding(end = 12.dp),
+                onClick = { component.acceptRequest(it) }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Done,
+                    contentDescription = "Accept Request",
+                    tint = colorScheme.primary
+                )
+            }
 
-            receivedStatus is Error -> Text(
-                text = (receivedStatus as Error).body.toString(),
-                modifier = Modifier.align(Alignment.Center)
-            )
-
-            receivedStatus == Success -> {
-                if (requests.reqs.isEmpty()) Text(
-                    text = "You haven't received any requests.",
-                    modifier = Modifier.align(Alignment.Center)
-                ) else ScrollLazyColumn(modifier = Modifier.fillMaxWidth()) {
-                    items(requests.reqs.toImmutableList()) {
-                        AddCard(
-                            label = "${it.requesterUsername.name} would like to add you!",
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            IconButton(
-                                modifier = Modifier.padding(end = 12.dp),
-                                onClick = { component.acceptRequest(it) }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Done,
-                                    contentDescription = "Accept Request",
-                                    tint = colorScheme.primary
-                                )
-                            }
-
-                            IconButton(
-                                modifier = Modifier.padding(end = 24.dp),
-                                onClick = { component.rejectRequest(it) }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Close,
-                                    contentDescription = "Reject Request",
-                                    tint = colorScheme.onError
-                                )
-                            }
-                        }
-                    }
-                }
+            IconButton(
+                modifier = Modifier.padding(end = 24.dp),
+                onClick = { component.rejectRequest(it) }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Reject Request",
+                    tint = colorScheme.onError
+                )
             }
         }
     }
