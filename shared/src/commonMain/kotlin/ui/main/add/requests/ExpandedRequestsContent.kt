077a632cebcbe2defe4056afd0142cf4e53e0c72
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-18 21:36:30 +0200

    Hoists Surface blocks into SelectionIcon composable

--- shared/src/commonMain/kotlin/ui/main/add/requests/ExpandedRequestsContent.kt
+++ shared/src/commonMain/kotlin/ui/main/add/requests/ExpandedRequestsContent.kt
@@ -1,14 +1,9 @@
 package ui.main.add.requests
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,14 +13,12 @@ import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.add.requests.RequestComponent
 import component.main.add.requests.RequestComponent.Child.ReceivedChild
 import component.main.add.requests.RequestComponent.Child.SentChild
-import kotlinx.collections.immutable.toImmutableList
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import ui.composables.expect.ScrollLazyColumn
 import ui.icons.ReceivedRequestsIcon
+import ui.icons.SelectionIcon
 import ui.icons.SentRequestsIcon
-import ui.main.add.AddCard
 import ui.main.add.requests.received.ReceivedContent
-import util.Status
+import ui.main.add.requests.sent.SentContent
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -46,16 +39,14 @@ fun ExpandedRequestsContent(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.End
             ) {
-                Surface(
-                    shape = CircleShape,
-                    color = if (it.instance is ReceivedChild) colorScheme.primaryContainer else colorScheme.background,
-                    modifier = Modifier.padding(end = 8.dp)
+                SelectionIcon(
+                    modifier = Modifier.padding(end = 8.dp),
+                    color = if (it.instance is ReceivedChild) colorScheme.primaryContainer else colorScheme.background
                 ) { ReceivedRequestsIcon { if (it.instance is SentChild) component.onReceivedRequestsSelected() } }
 
-                Surface(
-                    shape = CircleShape,
-                    color = if (it.instance is SentChild) colorScheme.primaryContainer else colorScheme.background,
-                    modifier = Modifier.padding(end = 8.dp)
+                SelectionIcon(
+                    modifier = Modifier.padding(end = 8.dp),
+                    color = if (it.instance is SentChild) colorScheme.primaryContainer else colorScheme.background
                 ) {
                     SentRequestsIcon {
                         if (it.instance is SentChild) component.onReceivedRequestsSelected()
@@ -68,55 +59,7 @@ fun ExpandedRequestsContent(
                 when (val child = it.instance) {
                     is ReceivedChild ->
                         ReceivedContent(component = child.component, snackbarHostState = snackbarHostState, modifier = Modifier.fillMaxSize())
-                    is SentChild -> {
-                        val requests by child.component.sentList.subscribeAsState()
-                        val sentStatus by child.component.listStatus.subscribeAsState()
-                        val sentLoading by child.component.listLoading.subscribeAsState()
-                        val cancelStatus by child.component.actionStatus.subscribeAsState()
-
-                        LaunchedEffect(requests) { child.component.getSentList() }
-                        LaunchedEffect(cancelStatus) { if (cancelStatus == Status.Success) child.component.getSentList() }
-
-                        Box(modifier = Modifier.fillMaxSize()) {
-                            when {
-                                sentLoading -> CircularProgressIndicator(
-                                    modifier = Modifier.size(120.dp).align(Alignment.Center),
-                                    color = colorScheme.primary,
-                                    trackColor = colorScheme.surfaceVariant
-                                )
-
-                                sentStatus is Status.Error -> Text(
-                                    text = (sentStatus as Status.Error).body.toString(),
-                                    modifier = Modifier.align(Alignment.Center)
-                                )
-
-                                sentStatus == Status.Success -> {
-                                    if (requests.reqs.isEmpty()) Text(
-                                        text = "You haven't sent any requests.",
-                                        modifier = Modifier.align(Alignment.Center)
-                                    ) else ScrollLazyColumn(modifier = Modifier.fillMaxWidth()) {
-                                        items(requests.reqs.toImmutableList()) {
-                                            AddCard(
-                                                label = "Sent to ${it.toUsername.name}",
-                                                modifier = Modifier.fillMaxWidth()
-                                            ) {
-                                                IconButton(
-                                                    modifier = Modifier.padding(end = 24.dp),
-                                                    onClick = { child.component.cancelRequest(it.toUsername) }
-                                                ) {
-                                                    Icon(
-                                                        imageVector = Icons.Default.Delete,
-                                                        contentDescription = "Cancel request",
-                                                        tint = colorScheme.primary
-                                                    )
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    is SentChild -> SentContent(component = child.component, modifier = Modifier.fillMaxSize())
                 }
             }
         }
