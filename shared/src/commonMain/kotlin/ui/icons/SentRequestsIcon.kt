3f09724bac7cee1f5836146a036323ea276afd54
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-18 20:35:54 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/icons/SentRequestsIcon.kt
@@ -0,0 +1,21 @@
+package ui.icons
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun SentRequestsIcon(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    IconButton(modifier = modifier, onClick = { onClick() }) {
+        Icon(
+            painter = painterResource("outgoing.xml"),
+            contentDescription = "Sent Requests",
+            tint = colorScheme.primary
+        )
+    }
+}
