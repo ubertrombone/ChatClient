5463613660c01073c6aa76b8d53e2a27010b73ec
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-21 21:26:32 +0200

    Adds coreData dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -53,6 +53,7 @@ kotlin {
             implementation(libs.bundles.ktor.common)
             implementation(libs.bundles.kotlinx.common)
             implementation(libs.sqldelight.coroutinesExtensions)
+            implementation(project(":coreData"))
         }
 
         androidMain.dependencies {
