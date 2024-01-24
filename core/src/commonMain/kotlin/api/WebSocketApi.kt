642b0353f30b99c800502d18ad6d12913c47002b
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-24 21:37:31 +0200

    Starts work putting this together

--- core/src/commonMain/kotlin/api/WebSocketApi.kt
+++ core/src/commonMain/kotlin/api/WebSocketApi.kt
@@ -1,6 +1,8 @@
 package api
 
+import api.model.ChatMessage
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
+import db.ChatRepository
 import io.ktor.client.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -8,12 +10,19 @@ import io.ktor.client.plugins.websocket.*
 import io.ktor.http.HttpMethod.Companion.Get
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.serialization.json.Json
 import settings.SettingsRepository
 import util.Constants.IP
 import util.Constants.PORT
 
-class WebSocketApi(private val settings: SettingsRepository) : InstanceKeeper.Instance {
+class WebSocketApi(
+    private val userInput: StateFlow<ChatMessage>,
+    private val settings: SettingsRepository,
+    private val chatRepository: ChatRepository
+) : InstanceKeeper.Instance {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
     private val client = HttpClient {
         install(WebSockets)
@@ -21,23 +30,36 @@ class WebSocketApi(private val settings: SettingsRepository) : InstanceKeeper.In
         defaultRequest { url(scheme = "ws", host = IP, port = PORT) }
     }
 
+    private val _incomingMessages = MutableSharedFlow<ChatMessage>()
+    val incomingMessages: SharedFlow<ChatMessage> = _incomingMessages
+
     init {
         scope.launch {
             client.webSocket(method = Get, path = "/chat") {
+                val incomingMessageRoutine = launch { outputMessages() }
+                val userInputRoutine = launch { inputMessages() }
 
+                userInputRoutine.join()
+                incomingMessageRoutine.cancelAndJoin()
             }
         }
     }
 
-    suspend fun DefaultClientWebSocketSession.outputMessages() {
-
+    private suspend fun DefaultClientWebSocketSession.outputMessages() {
+        try {
+            for (message in incoming) {
+                // TODO: Is this deserializing anything?
+                _incomingMessages.emit(receiveDeserialized<ChatMessage>().also(::println))
+            }
+        } catch (e: Exception) { println("ERROR: ${e.message}") }
     }
 
-    suspend fun DefaultClientWebSocketSession.inputMessages() {
-        while (true) {
+    private suspend fun DefaultClientWebSocketSession.inputMessages(): Unit =
+        userInput.collect { runCatching { sendSerialized(it) }.getOrNull() }
 
-        }
+
+    override fun onDestroy() {
+        client.close()
+        scope.cancel()
     }
-
-    override fun onDestroy() = scope.cancel()
 }
