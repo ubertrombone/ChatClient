8393d6eecb7564bb3e8d7c332fea2b7b7682a5cb
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-25 21:21:32 +0200

    Makes some progress on testing how this works

--- core/src/commonMain/kotlin/api/WebSocketApi.kt
+++ core/src/commonMain/kotlin/api/WebSocketApi.kt
@@ -3,39 +3,44 @@ package api
 import api.model.ChatMessage
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
 import db.ChatRepository
+import io.github.aakira.napier.Napier
 import io.ktor.client.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.websocket.*
-import io.ktor.http.HttpMethod.Companion.Get
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.serialization.json.Json
 import settings.SettingsRepository
 import util.Constants.IP
 import util.Constants.PORT
 
 class WebSocketApi(
-    private val userInput: StateFlow<ChatMessage>,
+    userInput: MutableSharedFlow<ChatMessage?>,
     private val settings: SettingsRepository,
     private val chatRepository: ChatRepository
 ) : InstanceKeeper.Instance {
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
     private val client = HttpClient {
-        install(WebSockets)
+        install(WebSockets) { contentConverter = KotlinxWebsocketSerializationConverter(Json) }
         install(ContentNegotiation) { json(json = Json { prettyPrint = true }) }
         defaultRequest { url(scheme = "ws", host = IP, port = PORT) }
     }
+
+    private val _userInput = userInput
+    suspend fun emitInput(message: ChatMessage) = _userInput.emit(message)
 
     private val _incomingMessages = MutableSharedFlow<ChatMessage>()
     val incomingMessages: SharedFlow<ChatMessage> = _incomingMessages
 
     init {
         scope.launch {
-            client.webSocket(method = Get, path = "/chat") {
+            client.webSocket(path = "/chat", request = { bearerAuth(settings.token.get()) }) {
                 val incomingMessageRoutine = launch { outputMessages() }
                 val userInputRoutine = launch { inputMessages() }
 
@@ -47,15 +52,24 @@ class WebSocketApi(
 
     private suspend fun DefaultClientWebSocketSession.outputMessages() {
         try {
-            for (message in incoming) {
-                // TODO: Is this deserializing anything?
-                _incomingMessages.emit(receiveDeserialized<ChatMessage>().also(::println))
+            incoming.consumeEach {
+                val m = receiveDeserialized<ChatMessage>()
+                println("MESSAGE: $m")
+                _incomingMessages.emit(m)
             }
         } catch (e: Exception) { println("ERROR: ${e.message}") }
     }
 
-    private suspend fun DefaultClientWebSocketSession.inputMessages(): Unit =
-        userInput.collect { runCatching { sendSerialized(it) }.getOrNull() }
+    private suspend fun DefaultClientWebSocketSession.inputMessages() {
+        while (true) {
+            _userInput.collect {
+                println("INPUT: $it")
+                runCatching { it?.let { sendSerialized(it) } }.getOrElse { throwable ->
+                    Napier.e(throwable.message ?: "An unknown error has occurred.")
+                }
+            }
+        }
+    }
 
 
     override fun onDestroy() {
