d0bb905fb88ae996945eaf14c4fb8041c26815f9
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-28 21:05:12 +0200

    Updates functions to match changes to tables; mapToOne calls have been refactored to mapToOneOrNull.

--- coreData/src/commonMain/kotlin/db/ChatRepository.kt
+++ coreData/src/commonMain/kotlin/db/ChatRepository.kt
@@ -2,7 +2,7 @@ package db
 
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
-import app.cash.sqldelight.coroutines.mapToOne
+import app.cash.sqldelight.coroutines.mapToOneOrNull
 import com.ubertrombone.chat.ChatDatabase
 import com.ubertrombone.chat.Chats
 import com.ubertrombone.chat.Messages
@@ -17,14 +17,14 @@ class ChatRepository(
     private val scope = CoroutineScope(dispatcher + SupervisorJob())
 
     fun getChats(): Flow<List<Chats>> = chats.chatsQueries.selectAll().asFlow().mapToList(dispatcher)
-    fun getChatById(id: Int): Flow<Chats> = chats.chatsQueries.selectChatByID(id.toLong()).asFlow().mapToOne(dispatcher)
-    fun selectChat(userOne: Int, userTwo: Int): Flow<Chats> =
-        chats.chatsQueries.selectChat(userOne.toLong(), userTwo.toLong()).asFlow().mapToOne(dispatcher)
-    suspend fun insertChat(userOne: Int, userTwo: Int) = withContext(scope.coroutineContext) {
-        chats.chatsQueries.insertChat(userOne.toLong(), userTwo.toLong())
+    fun getChatById(id: Int): Flow<Chats?> = chats.chatsQueries.selectChatByID(id.toLong()).asFlow().mapToOneOrNull(dispatcher)
+    fun selectChat(userOne: String, userTwo: String): Flow<Chats?> =
+        chats.chatsQueries.selectChat(userOne, userTwo).asFlow().mapToOneOrNull(dispatcher)
+    suspend fun insertChat(userOne: String, userTwo: String) = withContext(scope.coroutineContext) {
+        chats.chatsQueries.insertChat(userOne, userTwo)
     }
-    suspend fun updateChat(userOne: Int, userTwo: Int, id: Int) = withContext(scope.coroutineContext) {
-        chats.chatsQueries.updateChat(userOne = userOne.toLong(), userTwo = userTwo.toLong(), id = id.toLong())
+    suspend fun updateChat(userOne: String, userTwo: String, id: Int) = withContext(scope.coroutineContext) {
+        chats.chatsQueries.updateChat(userOne = userOne, userTwo = userTwo, id = id.toLong())
     }
     suspend fun deleteChat(id: Int) = withContext(scope.coroutineContext) {
         chats.chatsQueries.deleteChat(id.toLong())
@@ -33,23 +33,24 @@ class ChatRepository(
     fun getMessages(): Flow<List<Messages>> = chats.messagesQueries.selectAll().asFlow().mapToList(dispatcher)
     fun getMessagesFromChat(chatId: Int): Flow<List<Messages>> =
         chats.messagesQueries.selectAllFromChat(chatId.toLong()).asFlow().mapToList(dispatcher)
-    fun getMessage(id: Int): Flow<Messages> = chats.messagesQueries.selectCache(id.toLong()).asFlow().mapToOne(dispatcher)
+    fun getMessage(id: Int): Flow<Messages?> =
+        chats.messagesQueries.selectCache(id.toLong()).asFlow().mapToOneOrNull(dispatcher)
     suspend fun insertMessage(message: Message) = withContext(scope.coroutineContext) {
         chats.messagesQueries.insertCache(
             message = message.message,
-            sender = message.sender.toLong(),
+            sender = message.sender,
             timestamp = message.timestamp.epochSeconds,
-            primaryUserRef = message.primaryUserRef.toLong(),
+            primaryUserRef = message.primaryUserRef,
             chat = message.chat.toLong()
         )
     }
     suspend fun editMessage(message: Message) = withContext(scope.coroutineContext) {
         chats.messagesQueries.updateCache(
-            id = message.id.toLong(),
+            id = message.id?.toLong() ?: throw NullPointerException("Cannot edit Message with null ID."),
             message = message.message,
-            sender = message.sender.toLong(),
+            sender = message.sender,
             timestamp = message.timestamp.epochSeconds,
-            primaryUserRef = message.primaryUserRef.toLong(),
+            primaryUserRef = message.primaryUserRef,
             chat = message.chat.toLong()
         )
     }
