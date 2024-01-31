093ba4963cadfb44510532346198bde19daa8efb
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-31 21:21:41 +0200

    Initial Commit

---
+++ shared/src/commonMain/kotlin/ui/main/friends/ChatWindow.kt
@@ -0,0 +1,31 @@
+package ui.main.friends
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import component.main.friends.chat.ChatComponent
+
+@Composable
+fun ChatWindow(component: ChatComponent, modifier: Modifier = Modifier) {
+    val chat by component.messages.collectAsState(emptyList())
+
+    Box(modifier) {
+        Row(modifier = Modifier.fillMaxWidth().height(36.dp).align(Alignment.TopCenter).background(Color.Green)) {
+
+        }
+
+        Column(modifier = Modifier.fillMaxSize().padding(vertical = 36.dp).align(Alignment.Center).background(Color.Red)) {
+
+        }
+
+        Row(modifier = Modifier.fillMaxWidth().height(36.dp).align(Alignment.BottomCenter).background(Color.Cyan)) {
+
+        }
+    }
+}
