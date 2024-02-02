a06d53e7c4d3e88537c70ec5edcdbfd88e65ecaa
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-02 20:52:09 +0200

    Adds BottomBarSystemNavColor function

--- shared/src/commonMain/kotlin/ui/main/friends/ChatWindow.kt
+++ shared/src/commonMain/kotlin/ui/main/friends/ChatWindow.kt
@@ -2,6 +2,7 @@ package ui.main.friends
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -10,10 +11,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import component.main.friends.chat.ChatComponent
+import util.BottomBarSystemNavColor
 
 @Composable
 fun ChatWindow(component: ChatComponent, modifier: Modifier = Modifier) {
     val chat by component.messages.collectAsState(emptyList())
+
+    BottomBarSystemNavColor(colorScheme.background)
 
     Box(modifier) {
         Row(modifier = Modifier.fillMaxWidth().height(36.dp).align(Alignment.TopCenter).background(Color.Green)) {
