0f15968d1e88ed233e935630310eeae86058ec9e
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-03 15:01:49 +0200

    Starts adding UI components.

--- shared/src/commonMain/kotlin/ui/main/friends/ChatWindow.kt
+++ shared/src/commonMain/kotlin/ui/main/friends/ChatWindow.kt
@@ -1,67 +1,110 @@
 package ui.main.friends
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import component.main.friends.chat.ChatComponent
+import kotlinx.coroutines.launch
 import ui.icons.NavBackButton
 import util.BottomBarSystemNavColor
+import util.SoftInputMode
+import util.textFieldColors
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ChatWindow(component: ChatComponent, modifier: Modifier = Modifier) {
     val chat by component.messages.collectAsState(emptyList())
+    val userInput by component.userInput.subscribeAsState()
+    var input by remember(userInput) { mutableStateOf(userInput) }
+    var inputIsValid by remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+    val keyboardRequester = remember { BringIntoViewRequester() }
 
+    LaunchedEffect(input) { inputIsValid = input.isNotBlank() }
     BottomBarSystemNavColor(colorScheme.background)
+    SoftInputMode(false)
 
-    Box(modifier) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-                .height(50.dp)
-                .align(Alignment.TopCenter),
-            horizontalArrangement = Arrangement.SpaceBetween
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                NavBackButton { component.navBack() }
+                Text(
+                    text = component.friend.username.name,
+                    style = typography.headlineLarge,
+                    color = colorScheme.primary,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+                Icon(
+                    imageVector = Icons.Outlined.MoreVert,
+                    contentDescription = "Friend Profile",
+                    tint = colorScheme.primary,
+                    modifier = Modifier
+                        .padding(end = 12.dp, top = 12.dp)
+                        .size(40.dp)
+                        .padding(5.dp)
+                        .clip(CircleShape)
+                        .clickable { println("TODO") }
+                )
+            }
+        },
+        bottomBar = {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                OutlinedTextField(
+                    value = input,
+                    onValueChange = component::updateInput,
+                    colors = textFieldColors(),
+                    shape = RoundedCornerShape(12.dp),
+                    placeholder = { Text(text = "Message") },
+                    maxLines = 5,
+                    keyboardOptions = KeyboardOptions(
+                        autoCorrect = true,
+                        imeAction = ImeAction.None,
+                        capitalization = KeyboardCapitalization.Sentences,
+                    ),
+                    modifier = Modifier
+                        .weight(8f)
+                        .bringIntoViewRequester(keyboardRequester)
+                        .onFocusEvent {
+                            if (it.isFocused) {
+                                scope.launch { keyboardRequester.bringIntoView() }
+                            }
+                        }
+                )
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NavBackButton { component.navBack() }
-            Text(
-                text = component.friend.username.name,
-                style = typography.headlineLarge,
-                color = colorScheme.primary,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding(top = 12.dp)
-            )
-            Icon(
-                imageVector = Icons.Outlined.MoreVert,
-                contentDescription = "Friend Profile",
-                tint = colorScheme.primary,
-                modifier = Modifier
-                    .padding(end = 12.dp, top = 12.dp)
-                    .size(40.dp)
-                    .padding(5.dp)
-                    .clip(CircleShape)
-                    .clickable { println("TODO") }
-            )
-        }
-
-        Column(modifier = Modifier.fillMaxSize().padding(vertical = 50.dp).align(Alignment.Center).background(Color.Red)) {
-
-        }
-
-        Row(modifier = Modifier.fillMaxWidth().height(50.dp).align(Alignment.BottomCenter).background(Color.Cyan)) {
 
         }
     }
