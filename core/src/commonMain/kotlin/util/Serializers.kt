a90599c9491b4aa84bc55ac3737845b5b4dd46dd
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-18 20:24:49 +0200

    Adds serializer for ImmutableList

--- core/src/commonMain/kotlin/util/Serializers.kt
+++ core/src/commonMain/kotlin/util/Serializers.kt
@@ -3,12 +3,15 @@
 
 package util
 
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.collections.immutable.toPersistentList
 import kotlinx.collections.immutable.toPersistentSet
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Serializer
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.builtins.SetSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.serialDescriptor
@@ -16,6 +19,21 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 typealias SerializableImmutableSet<T> = @Serializable(ImmutableSetSerializer::class) ImmutableSet<T>
+typealias SerializableImmutableList<T> = @Serializable(ImmutableListSerializer::class) ImmutableList<T>
+
+@Serializer(forClass = ImmutableList::class)
+class ImmutableListSerializer<T>(private val dataSerializer: KSerializer<T>) : KSerializer<ImmutableList<T>> {
+    private class PersistentListDescriptor : SerialDescriptor by serialDescriptor<List<String>>() {
+        override val serialName: String = "kotlinx.serialization.immutable.ImmutableList"
+    }
+    override val descriptor: SerialDescriptor = PersistentListDescriptor()
+
+    override fun serialize(encoder: Encoder, value: ImmutableList<T>) =
+        ListSerializer(dataSerializer).serialize(encoder, value.toList())
+
+    override fun deserialize(decoder: Decoder): ImmutableList<T> =
+        ListSerializer(dataSerializer).deserialize(decoder).toPersistentList()
+}
 
 @Serializer(forClass = ImmutableSet::class)
 class ImmutableSetSerializer<T>(private val dataSerializer: KSerializer<T>) : KSerializer<ImmutableSet<T>> {
