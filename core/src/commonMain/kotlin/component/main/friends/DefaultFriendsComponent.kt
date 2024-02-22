1e7a0c2ea2cbb0693449db8e8efb366e2bf7ebbf
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-22 20:47:47 +0200

    Adds chats property and init block to collect chatRequests.

--- core/src/commonMain/kotlin/component/main/friends/DefaultFriendsComponent.kt
+++ core/src/commonMain/kotlin/component/main/friends/DefaultFriendsComponent.kt
@@ -1,9 +1,11 @@
 package component.main.friends
 
 import api.ApplicationApi
+import api.ChatApi
 import api.ChatRequestApi
 import api.WebSocketApi
 import api.model.FriendInfo
+import api.model.OpenChatRequest
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.slot.*
 import com.arkivanov.decompose.value.Value
@@ -11,6 +13,10 @@ import component.main.friends.chat.ChatComponent
 import component.main.friends.chat.DefaultChatComponent
 import component.main.friends.model.FriendsSet
 import db.ChatRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import settings.SettingsRepository
 import util.Status
@@ -26,9 +32,13 @@ class DefaultFriendsComponent(
     override val friendsModel: FriendsModelImpl,
     override val logout: () -> Unit
 ) : FriendsComponent, ComponentContext by componentContext {
+    private val scope = CoroutineScope(Dispatchers.IO)
     override val friends: Value<FriendsSet> = friendsModel.friendsListState
     override val isLoading: Value<Boolean> = friendsModel.friendsListLoading
     override val status: Value<Status> = friendsModel.friendsListStatus
+
+    // TODO: collect all ChatAPI instances here
+    private val chats = LinkedHashSet<ChatApi>()
 
     // TODO: Have some kind of profile viewer here where a user will be able to block/remove a friend from their friends
     // TODO: Also consider making these actions possible within a FriendCard
@@ -42,8 +52,8 @@ class DefaultFriendsComponent(
             DefaultChatComponent(
                 componentContext = childComponentContext,
                 server = server,
-                webSocket = webSocket,
-                chatRequests = chatRequests,
+//                webSocket = webSocket,
+//                chatRequests = chatRequests,
                 chatRepository = chatRepository,
                 username = settings.username.get().toUsername(),
                 cache = settings.cache.get().toBooleanStrict(),
@@ -53,8 +63,21 @@ class DefaultFriendsComponent(
         }
     override val chatSlot: Value<ChildSlot<*, ChatComponent>> = _chatSlot
 
-    override fun showChat(user: FriendInfo) = chatNavigation.activate(ChatConfig(user))
+    override fun showChat(user: FriendInfo) = chatNavigation.activate(ChatConfig(user)).also {
+        scope.launch {
+            chatRequests.emitRequest(OpenChatRequest(
+                sender = settings.username.get().toUsername(),
+                recipient = user.username
+            ))
+        }
+    }
     override fun dismissChat() = chatNavigation.dismiss()
+
+    init {
+        scope.launch {
+            chatRequests.chats.collect(chats::add)
+        }
+    }
 
     @Serializable
     private data class ChatConfig(val user: FriendInfo)
