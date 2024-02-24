cba5ff3d3c4b5117052bde53bcb71fa944ffb81f
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-24 13:22:39 +0200

    Adds request property.

--- core/src/commonMain/kotlin/api/model/ChatEndPointResponse.kt
+++ core/src/commonMain/kotlin/api/model/ChatEndPointResponse.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ChatEndPointResponse(
     val success: Boolean,
-    val chatId: Int
+    val chatId: Int,
+    val request: OpenChatRequest?
 )
